d7a08e84da7fe057c2ca0b17f6350cdb35fe5f66
vigilantewolfMS <32073374+vigilantewolfMS@users.noreply.github.com>
2018-04-10 13:27:31 -0400

    owo whats this

---
+++ starter/SuperSoyBoy/Assets/CameraLerpToTransform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLerpToTransform : MonoBehaviour {
+
+	public Transform camTarget;
+	public float trackingSpeed;
+	public float minX, minY, maxX, maxY;
+
+	private void FixedUpdate()
+	{
+		if (camTarget != null) {
+			var newPos = Vector2.Lerp(transform.position, camTarget.position, Time.deltaTime * trackingSpeed);
+			var camPos = new Vector3 (newPos.x, newPos.y, -10f);
+			var v3 = camPos;
+			var clampX = Mathf.Clamp (v3.x, minX, maxX);
+			var clampY = Mathf.Clamp (v3.y, minY, maxY);
+			transform.position = new Vector3 (clampX, clampY, -10f);
+		}
+	}
+}
---
+++ starter/SuperSoyBoy/Assets/SoyboyController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(SpriteRenderer),typeof(Rigidbody2D), typeof(Animator))]
+
+public class SoyboyController : MonoBehaviour {
+	public float speed = 14f;
+	public float accel= 6f;
+	private Vector2 input;
+	private SpriteRenderer sr;
+	private Rigidbody2D rb;
+	private Animator animator;
+
+	void Awake()
+	{
+		sr = GetComponent<SpriteRenderer> ();
+		animator = GetComponent<Animator> ();
+		rb = GetComponent<Rigidbody2D> ();
+	}
+		
+	void FixedUpdate(){
+		var acceleration = accel;
+		var xVelocity = 0f;
+		if (input.x == 0)
+		{
+			xVelocity = 0f;
+		}
+		else
+		{
+			xVelocity = rb.velocity.x;
+		}
+		rb.AddForce (new Vector2 (((input.x * speed) - rb.velocity.x) * acceleration, 0));
+		rb.velocity = new Vector2 (xVelocity, rb.velocity.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		input.x = Input.GetAxis ("Horizontal");
+		input.y = Input.GetAxis ("Jump");
+
+		if (input.x > 0f) {
+			
+			sr.flipX = false;
+		} 
+
+		else if (input.x < 0f) {
+			
+			sr.flipX = true;
+		}
+		
+	}
+}
